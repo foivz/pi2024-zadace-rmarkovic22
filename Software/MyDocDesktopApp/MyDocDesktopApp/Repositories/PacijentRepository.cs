8690be241a2b45e08c845c96918be8249b44948e
Rea Markovic <rmarkovic22@student.foi.hr>
2024-05-31 18:04:58 +0200

    Povezana klasa PacijentRepository s bazom

--- Software/MyDocDesktopApp/MyDocDesktopApp/Repositories/PacijentRepository.cs
+++ Software/MyDocDesktopApp/MyDocDesktopApp/Repositories/PacijentRepository.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using DBLayer;
+using MyDocDesktopApp.models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyDocDesktopApp.Repositories
 {
-    internal class PacijentRepository
+    public class PacijentRepository
     {
+        public static Pacijent GetPacijent(int id)
+        {
+            Pacijent pacijent = null;
+            string sql = $"SELECT * FROM Pacijent WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                pacijent = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return pacijent;
+        }
+
+        private static Pacijent CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string Ime = reader["Ime"].ToString();
+            string Prezime = reader["Prezime"].ToString();
+            string Email = reader["email"].ToString();
+            string Kontakt = reader["kontakt"].ToString();    
+            var pacijent = new Pacijent
+            {
+                Id = id,
+                Ime = Ime,
+                Prezime = Prezime,
+                Email = Email,
+                Kontakt = Kontakt
+                
+            };
+            return pacijent;
+        }
+
     }
+
 }
